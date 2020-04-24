3d815893690c99575ade222ecb0588ac68fe40b9
binayakdev <joshibinayak17@gmail.com>
2020-04-24 15:53:20 +0545

    Function 3 and 4 added. Still changes to be made.

--- Semester2Coursework/Controllers/ReportController.cs
+++ Semester2Coursework/Controllers/ReportController.cs
@@ -7,6 +7,7 @@ using System.Web;
 using System.Web.Mvc;
 using Semester2Coursework.Data;
 using Semester2Coursework.Models;
+using Microsoft.Ajax.Utilities;
 
 namespace Semester2Coursework.Controllers
 {
@@ -15,6 +16,8 @@ namespace Semester2Coursework.Controllers
         private DataContext db = new DataContext();
         // GET: Report
         
+
+        // Function 1
         public ActionResult FilterAblumCountByArtist(string searchString)
         {
             var artistAlbums = db.ArtistAlbums.Include(a => a.Albums).Include(a => a.Artists);
@@ -25,6 +28,8 @@ namespace Semester2Coursework.Controllers
 
             return View(artistAlbums.ToList());
         }
+
+        // Function 2
         public ActionResult FilterAblumSelvedCountByArtist(string searchString)
         {
             
@@ -35,6 +40,34 @@ namespace Semester2Coursework.Controllers
             artistAlbums = artistAlbums.Where((s => s.Albums.SongCount > 0));
 
             return View(artistAlbums.ToList());
+        }
+
+        
+        // Function 3
+        public ActionResult MemberLoan(int? memberNumber, string searchString)
+        {
+            var constraint = DateTime.Now.AddDays(-31);
+            var loan = db.Loans.Include(a => a.Albums).Include(a => a.Members);
+
+            if (memberNumber.HasValue)
+            {
+                loan = loan.Where(s => s.Members.Id.Equals(memberNumber));
+            }
+            else if(!String.IsNullOrEmpty(searchString))
+            {
+                loan = loan.Where(s => s.Members.Name.Contains(searchString));
+            }
+            loan = loan.Where(s => s.IssueDate >= constraint);
+
+            return View(loan.ToList());
+        }
+
+        // Function 4
+        public ActionResult DvdDetails()
+        {
+            var albumProducer = db.AlbumProducers.Include(a => a.Albums).Include(a => a.Producers);
+
+            return View(albumProducer.ToList());
         }
 
     }
